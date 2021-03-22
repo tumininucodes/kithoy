04a4d04b88fa7982697bb3163a8bdb840d0e06f5
Tumi <oluwatumininuojo25@gmail.com>
2021-03-22 04:00:21 +0100

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shrine/main.dart';
+import 'file:///C:/Users/Tumi/Kotlin/kithoy/lib/default/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
